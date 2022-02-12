90777ac3c3f7a28da1b015833206835210df1e6a
Cho SangHyun <98shcho@naver.com>
2022-02-12 11:55:28 +0900

    Dynamic Programming

--- 백준/11726-2xn 타일링.py
+++ 백준/11726-2xn 타일링.py
@@ -1,13 +1,24 @@
+# N = int(input())
+
+# MOD = 10007
+# # dp[x][0] : x번째 열까지 채우는데 마지막이 세로로 끝나는 경우
+# # dp[x][1] : x번째 열까지 채우는데 마지막이 가로로 끝나는 경우
+# dp = [[0, 0], [1, 0], [1, 1], [2, 1]]
+
+# for i in range(4, 1001):
+#     dp.append(
+#         [(sum(dp[i - 2]) + dp[i - 1][1]) % MOD, sum(dp[i - 2]) % MOD]
+#     )
+
+# print(sum(dp[N]) % MOD)
+
 N = int(input())
 
 MOD = 10007
-# dp[x][0] : x번째 열까지 채우는데 마지막이 세로로 끝나는 경우
-# dp[x][1] : x번째 열까지 채우는데 마지막이 가로로 끝나는 경우
-dp = [[0, 0], [1, 0], [1, 1], [2, 1]]
 
-for i in range(4, 1001):
-    dp.append(
-        [(sum(dp[i - 2]) + dp[i - 1][1]) % MOD, sum(dp[i - 2]) % MOD]
-    )
+dp = [0, 1, 2, 3]
 
-print(sum(dp[N]) % MOD)
+for i in range(4, N + 1):
+    dp.append((dp[i - 2] + dp[i - 1]) % MOD)
+
+print(dp[N])
